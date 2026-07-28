0cef7fd48911ee0e55673e173c3c35e58c0e12b4
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-07-28 15:29:03 -0300

    feat: adiciona componente 'card'

---
+++ techmind-frontend/src/app/shared/card/card.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './card.html',
+})
+export class Card {
+  titleCard = input.required<string>()
+  
+  subtitle = input<string>('')
+  imageUrl = input<string>('')
+  
+  articleText = input<string>('')
+  authorName = input<string>('')
+
+}
