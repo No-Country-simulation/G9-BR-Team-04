80d48ae70ae6982b9abdc129354277ebf6352ddc
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-04 16:03:29 -0300

    feat: adicionadas pag's a 'Routes'

--- techmind-frontend/src/app/app.routes.ts
+++ techmind-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './pages/dashboard-page/dashboard-page';
+import { NotFoundPage } from './pages/notfound-page/notfound-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: DashboardPage },
+
+    { path: '**', component: NotFoundPage },
+]
