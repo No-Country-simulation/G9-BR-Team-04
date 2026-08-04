833416957ac732f4927acd84281930452bbf1603
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-04 16:48:39 -0300

    refactor: removido import do Dashboard

--- techmind-frontend/src/app/app.ts
+++ techmind-frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Dashboard } from './features/dashboard/dashboard';
 import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Dashboard],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
 })
 export class App {
