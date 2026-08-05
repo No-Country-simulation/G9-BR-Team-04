6cd3b36cac29bc5d29563e7ccd7b15d237faf0ee
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-05 18:10:02 -0300

    feat: adiciona icones a navegação mobile do 'header'

--- techmind-frontend/src/app/layout/header/header.ts
+++ techmind-frontend/src/app/layout/header/header.ts
@@ -3,20 +3,24 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Bars } from '@primeicons/angular/bars';
 import { ChartBar } from '@primeicons/angular/chart-bar';
 import { Home } from '@primeicons/angular/home';
-import { PAGES_LIST, PagesList } from '../../core/constants/pages-list';
+import { PIcon } from '@primeicons/angular/p-icon';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, Bars, Home, ChartBar
-  ],
+  imports: [RouterLink, RouterLinkActive, PIcon, Bars, Home, ChartBar],
   templateUrl: './header.html',
 })
 export class Header {
   logoURL: string = '/imgs/logo_techmind--no-bg.png'
   imgAlt: string = 'Logo TechMind'
 
-  pagesList: PagesList[] = PAGES_LIST
+  pagesList = [
+    { icon: 'home', label: 'Início', path: '' },
+
+    { icon: 'chart-bar', label: 'Dashboard', path: '' },
+  ]
 
   sidebarOpen = signal(false)
 
