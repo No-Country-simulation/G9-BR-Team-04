ef33fbeff1a8feec484af2235bebfecc2cf448fb
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-04 21:52:02 -0300

    refactor: ajusta proporções do 'user-avatar' e remove icone 'user'

--- techmind-frontend/src/app/shared/user-avatar/user-avatar.ts
+++ techmind-frontend/src/app/shared/user-avatar/user-avatar.ts
@@ -1,15 +1,13 @@
 import { Component, input } from '@angular/core';
-import { User } from '@primeicons/angular/user';
 
 @Component({
   selector: 'app-user-avatar',
-  imports: [User],
+  imports: [],
   templateUrl: './user-avatar.html',
 })
 export class UserAvatar {
 
   contributorInitials = input<string>('')
-
   contributorName = input<string>('')
 
 }
