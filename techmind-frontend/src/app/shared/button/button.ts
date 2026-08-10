dbe45790234a75f3879710f923a65e559a793cee
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-10 14:56:49 -0300

    refactor: adiciona props no componente 'button'

--- techmind-frontend/src/app/shared/button/button.ts
+++ techmind-frontend/src/app/shared/button/button.ts
@@ -1,13 +1,18 @@
 import { Component, input } from '@angular/core';
+import { PIcon } from '@primeicons/angular/p-icon';
 
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [PIcon],
   templateUrl: './button.html',
 })
 export class Button {
 
+  iconLeft = input<string>('')
+
   textButton = input.required<string>()
+
+  iconRight = input<string>('')
 
 }
